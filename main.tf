0380378e6c2dbcb89534f9c97e7aaae7cc2b93f0
hecalzu <46231281+hecalzu@users.noreply.github.com>
2026-03-24 11:21:19 -0600

    security groups test

--- main.tf
+++ main.tf
@@ -18,11 +18,15 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+data "aws_vpc" "default"{
+  default = true
 }
 
 module "blog_sg" {
@@ -37,4 +41,47 @@ module "blog_sg" {
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]  
+}
+
+resource "aws_security_group" "blog"{
+  name = "blog"
+  descriptioon = "Allow http and https in. Allow everything out"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group" "blog_http_in" { 
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+
+}
+
+resource "aws_security_group" "blog_http2_in" { 
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+
+}
+
+resource "aws_security_group" "blog_all_out" { 
+  type      = "egress"
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+
 }
