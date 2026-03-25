e87bab15da8e1bceecaf63d26fdfe491b3848c24
hecalzu <46231281+hecalzu@users.noreply.github.com>
2026-03-24 18:02:35 -0600

    reverting fixes

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
 
   filter {
@@ -11,20 +11,17 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["137112412989"] # Amazon
+  owners = ["979382823631"] # Bitnami
 }
 
-module "blog_vpc"{
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
@@ -32,101 +29,64 @@ module "blog_vpc"{
   }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.1"
-  name    = "blog"
 
-  vpc_id = module.blog_vpc.vpc_id
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  name = "blog"
 
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]  
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
+  name = "blog-alb"
 
-  security_groups = [module.blog_sg.security_group_id]
-    
-  listeners = {
-    blog-http = {
-      port     = 80
-      protocol = "HTTP"
-      forward = {
-        target_group_arn = aws_lb_target_group.blog.arn
-      }
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
     }
-  }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
   }
 }
 
-resource "aws_lb_target_group" "blog" {
-  name     = "blog"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.blog_vpc.vpc_id
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200-399"
-    healthy_threshold   = 2
-    unhealthy_threshold = 3
-    timeout             = 5
-    interval            = 30
-  }
-}
-
-module "blog_autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.2.0"
-            
-  name = "blog"
-
-  min_size = 1
-  max_size = 2
-  desired_capacity = 1
-  health_check_type         = "ELB"
-  health_check_grace_period = 600
-
-  # Place app instances in private subnets (egress through NAT) behind public ALB
-  vpc_zone_identifier = module.blog_vpc.private_subnets
-
-  launch_template_name = "blog"
-
-  security_groups = [module.blog_sg.security_group_id]
-
-  instance_type = var.instance_type
-  image_id      = data.aws_ami.app_ami.id
-
-  # Bootstrap Apache so ALB target health checks pass and traffic can be served
-  user_data = base64encode(<<-EOT
-#!/bin/bash
-set -xe
-mkdir -p /var/www/html
-echo "<h1>Blog app is running on $(hostname -f)</h1>" > /var/www/html/index.html
-
-# Avoid package-install/network dependency during bootstrap:
-# start a simple local HTTP server on port 80
-nohup python3 -m http.server 80 --directory /var/www/html >/var/log/simple-http.log 2>&1 &
-sleep 2
-curl -I http://127.0.0.1/ || true
-EOT
-  )
-
-  traffic_source_attachments = {
-    blob-alb = {
-      traffic_source_identifier = aws_lb_target_group.blog.arn
-    }
-  }
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
